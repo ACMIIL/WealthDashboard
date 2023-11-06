78972e2e5400d6d222c78d34a297ff9f12ee5493
TalhaSheikh7 <mohmadtalha.sheikh@acm.co.in>
2023-11-06 16:15:31 +0530

    layout

--- WealthDashboard/Controllers/MutualFundController.cs
+++ WealthDashboard/Controllers/MutualFundController.cs
@@ -8,19 +8,31 @@ namespace WealthDashboard.Controllers
         {
             return View();
         }
+        [Route("MutualFund/Main")]
         public IActionResult Mutual_Fund()
         {
             return View();
         }
+        [Route("MutualFund/Event")]
         public IActionResult Mf_sub_event_mf()
         {
             return View();
         }
+
+        [Route("MutualFund/Screener")]
         public IActionResult Mf_event_mf_screener()
         {
             return View();
         }
+
+        [Route("MutualFund/AdvicedFund")]
         public IActionResult Mf_event_mf_Advised_Fund()
+        {
+            return View();
+        }
+
+        [Route("MutualFund/GlobalSearch")]
+        public IActionResult MFGlobalSearch()
         {
             return View();
         }
